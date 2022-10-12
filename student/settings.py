460aab41ec22576e3f202bfe27deb7bff2db49da
therealkishan <kishanku960@gmail.com>
2022-10-12 13:44:11 +0530

    Update settings.py

--- student/settings.py
+++ student/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(!$x4)xyxl&7c%u1eew(fkb9+__fgb2xh#+d_r4o@yyqo)p5q5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','student-portal-system.herokuapp.com']
 
 
 # Application definition
