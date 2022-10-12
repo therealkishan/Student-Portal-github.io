50750175783d3470ce5f42fa6bf31bdb0ed81093
therealkishan <kishanku960@gmail.com>
2022-10-12 14:37:45 +0530

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from dashboard import views as dash_view
+from django.views.static import serve
+from django.conf.urls import url
 
 from django.contrib.auth import views as auth_views
 
@@ -28,7 +30,10 @@ urlpatterns = [
     path('profile/', dash_view.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='dashboard/logout.html'), name='logout'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
